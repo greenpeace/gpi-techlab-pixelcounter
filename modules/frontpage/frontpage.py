64f1c231aea899636cf123d12cc6119ec98fc0fc
Torbjorn Zetterlund <torbjorn.zetterlund@greenpeace.org>
2026-09-22 19:21:07 +0200

    update removed wrong routes

--- modules/frontpage/frontpage.py
+++ modules/frontpage/frontpage.py
@@ -30,38 +30,6 @@ def contactus():
     return render_template('contactus.html', **locals())
 
 
-#
-# API Route Default displays a webpage
-#
-
-
-@frontpageblue.route("/blogsingle")
-def blogsingle():
-    return render_template('blogsingle.html', **locals())
-
-#
-# API Route Default displays a webpage
-#
-
-
-@frontpageblue.route("/blogcard")
-def blogcard():
-    return render_template('blogcard.html', **locals())
-
-#
-# API Route Default displays a webpage
-#
-
-
-@frontpageblue.route("/career")
-def career():
-    return render_template('career.html', **locals())
-
-#
-# API Route Default displays a webpage
-#
-
-
 @frontpageblue.route("/privacypolicy")
 def privacypolicy():
     return render_template('privacypolicy.html', **locals())
@@ -83,39 +51,3 @@ def termsandconditions():
 @frontpageblue.route("/faq")
 def faq():
     return render_template('faq.html', **locals())
-
-#
-# API Route Default displays a webpage
-#
-
-
-@frontpageblue.route("/techlab")
-def techlab():
-    return render_template('techlab.html', **locals())
-
-#
-# API Route Default displays a webpage
-#
-
-
-@frontpageblue.route("/datalab")
-def datalab():
-    return render_template('datalab.html', **locals())
-
-#
-# API Route Default displays a webpage
-#
-
-
-@frontpageblue.route("/sciencelab")
-def sciencelab():
-    return render_template('sciencelab.html', **locals())
-
-#
-# API Route Default displays a webpage
-#
-
-
-@frontpageblue.route("/medialab")
-def medialab():
-    return render_template('medialab.html', **locals())
